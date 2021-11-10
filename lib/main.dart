e9d14499f062a1e0ab353671b5bfcfef3789afe7
VinuBalagopalAP <vinubalagopalap@gmail.com>
2021-11-10 20:08:15 +0530

    row with scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,86 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
+          leading: const Icon(Icons.home),
+          actions: [
+            IconButton(
+              onPressed: () {
+                print('Button clicked');
+              },
+              icon: Icon(Icons.arrow_left),
+            ),
+          ],
           title: Text('Material App Bar'),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                  vertical: 24.0,
+                ),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  color: Color(0xFF000000),
+                ),
+                child: const Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'Hello World',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                  vertical: 24.0,
+                ),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  color: Colors.amber,
+                ),
+                child: const Center(
+                  child: Text(
+                    'Hello World',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Image.network(
+                  'https://mbcet.ac.in/wp-content/uploads/2019/04/mbcet.jpg'),
+              const SizedBox(
+                height: 50.0,
+              ),
+              Image.asset('assets/mbcet.jpg'),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Image.network(
+                  'https://mbcet.ac.in/wp-content/uploads/2019/04/mbcet.jpg'),
+              const SizedBox(
+                height: 50.0,
+              ),
+              Image.asset('assets/mbcet.jpg'),
+            ],
           ),
         ),
       ),
